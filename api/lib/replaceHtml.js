9e791f14ae2af12c3280fe8c89e7ff92eb2c51e0
Fernando Mendes <fernando@mendes.codes>
2020-07-08 19:04:45 +0100

    Alert after 10s

--- api/lib/replaceHtml.js
+++ api/lib/replaceHtml.js
@@ -1,7 +1,19 @@
 const axios = require("axios");
 const absolutify = require("absolutify");
+const cheerio = require("cheerio");
 
 const url = require("url");
+
+const alertText =
+  "Feeling outraged? Think again!\\nThis article was originally written about Trump.\\nObamaDidIt is a website that replaces the word \"Trump\" with the word \"Obama\".\\nIf this outraged you, I would reflect on your own biases.";
+
+const alert = `
+  <script>
+    setTimeout(function() {
+      alert('${alertText}')
+    }, 10000);
+  </script>
+`;
 
 module.exports = async function replaceHTML(targetUrl) {
   const { data } = await axios.get(targetUrl);
@@ -11,5 +23,9 @@ module.exports = async function replaceHTML(targetUrl) {
 
   const html = absolutify(data, rootUrl).replace(/trump/gi, "Obama");
 
-  return html;
+  const $ = cheerio.load(html);
+
+  $("body").append(alert);
+
+  return $.html();
 };
