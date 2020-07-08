2e8bc18db1e74606451f25ffb5710f69c3d5c73a
Fernando Mendes <fernando@mendes.codes>
2020-07-08 21:10:44 +0100

    Replace 'Donald' with 'Barack'

--- api/lib/replaceHtml.js
+++ api/lib/replaceHtml.js
@@ -10,7 +10,9 @@ module.exports = async function replaceHTML(targetUrl) {
   const parsedUrl = url.parse(targetUrl);
   const rootUrl = `${parsedUrl.protocol}//${parsedUrl.hostname}`;
 
-  const html = absolutify(data, rootUrl).replace(/trump/gi, "Obama");
+  const html = absolutify(data, rootUrl)
+    .replace(/trump/gi, "Obama")
+    .replace(/donald/gi, "Barack");
 
   return appendModal(html, targetUrl);
 };
