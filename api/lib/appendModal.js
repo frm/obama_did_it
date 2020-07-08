a63fbf543add31eac5469c15b0c17383ea98751f
Fernando Mendes <fernando@mendes.codes>
2020-07-08 20:14:53 +0100

    Make the modal obvious

---
+++ api/lib/appendModal.js
@@ -0,0 +1,47 @@
+const cheerio = require("cheerio");
+
+const modalStyle = `
+<style>
+  .modal {
+    display: none;
+    position: fixed;
+    z-index: 99;
+    padding: 100px 26px;
+    left: 0;
+    top: 0;
+    width: 100vw;
+    height: 100vh;
+    background-color: #fff;
+  }
+</style>
+`;
+
+const modalMarkup = (url) => `
+<div class="modal">
+  <p>Feeling outraged? Think again!</p>
+  <p>This article was originally written about Trump.</p>
+  <br/>
+  <p>ObamaDidIt is a website that replaces the word "Trump" with the word "Obama".</p>
+  <p>If this outraged you, you should reflect on your own biases.</p>
+  <br />
+  <p>See for yourself. Find the original at <a href="${url}">${url}</a></p>
+</div>
+`;
+
+const modalScript = `
+<script>
+  setTimeout(function() {
+    document.getElementsByClassName("modal")[0].style.display = "block";
+  }, 7000);
+</script>
+`;
+
+module.exports = async function appendModal(html, url) {
+  const $ = cheerio.load(html);
+
+  $("body").append(modalMarkup(url));
+  $("body").append(modalScript);
+  $("body").append(modalStyle);
+
+  return $.html();
+};
--- api/lib/replaceHtml.js
+++ api/lib/replaceHtml.js
@@ -1,19 +1,8 @@
 const axios = require("axios");
 const absolutify = require("absolutify");
-const cheerio = require("cheerio");
+const appendModal = require("./appendModal");
 
 const url = require("url");
-
-const alertText =
-  "Feeling outraged? Think again!\\nThis article was originally written about Trump.\\nObamaDidIt is a website that replaces the word \"Trump\" with the word \"Obama\".\\nIf this outraged you, I would reflect on your own biases.";
-
-const alert = `
-  <script>
-    setTimeout(function() {
-      alert('${alertText}')
-    }, 10000);
-  </script>
-`;
 
 module.exports = async function replaceHTML(targetUrl) {
   const { data } = await axios.get(targetUrl);
@@ -23,9 +12,5 @@ module.exports = async function replaceHTML(targetUrl) {
 
   const html = absolutify(data, rootUrl).replace(/trump/gi, "Obama");
 
-  const $ = cheerio.load(html);
-
-  $("body").append(alert);
-
-  return $.html();
+  return appendModal(html, targetUrl);
 };
