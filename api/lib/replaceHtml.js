089667798c04ea96129802921aa4878b39b6d36c
Fernando Mendes <fernando@mendes.codes>
2020-07-25 15:33:25 +0100

    Actually swap the names

--- api/lib/replaceHtml.js
+++ api/lib/replaceHtml.js
@@ -1,8 +1,10 @@
 const axios = require("axios");
 const absolutify = require("absolutify");
+const url = require("url");
+
 const appendOverlay = require("./appendOverlay");
 
-const url = require("url");
+const randomString = () => Math.random().toString(36).substring(2, 15);
 
 module.exports = async function replaceHTML(targetUrl) {
   const { data } = await axios.get(targetUrl);
@@ -10,9 +12,16 @@ module.exports = async function replaceHTML(targetUrl) {
   const parsedUrl = url.parse(targetUrl);
   const rootUrl = `${parsedUrl.protocol}//${parsedUrl.hostname}`;
 
+  const barack = randomString();
+  const obama = randomString();
+
   const html = absolutify(data, rootUrl)
+    .replace(/barack/gi, barack)
+    .replace(/obama/gi, obama)
+    .replace(/donald/gi, "Barack")
     .replace(/trump/gi, "Obama")
-    .replace(/donald/gi, "Barack");
+    .replace(new RegExp(barack, "g"), "Donald")
+    .replace(new RegExp(obama, "g"), "Trump");
 
   return appendOverlay(html, targetUrl);
 };
